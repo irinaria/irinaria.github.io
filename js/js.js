5d0ca90df72600fbddc82eb7057ccc273b85824e
irinaria <irenegouret@gmail.com>
2021-09-08 15:27:01 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    // toggle mobile menu
+    $('.icon-menu').click(function () {
+        $('body').toggleClass('menu-shown');
+        $('.header__menu').slideToggle(400);
+
+        // $('.header_wrapper').animate({
+        //     scrollTop: 0
+        // }, 300);
+    });
+
+
+    // custom selectbox
+    $(".selectbox")
+        .select2({
+            placeholder: "Выберите тип системы",
+            //allowClear: true,
+            minimumResultsForSearch: Infinity
+        })
+        .on('select2:open', function () {
+            var optionsContainer = $('.select2-results__options').get(0);
+
+            setTimeout(function () {
+                new SimpleBar(optionsContainer, {
+                    autoHide: false
+                });
+            }, 0);
+        });
+
+
+    // range slider
+    var rangeSlider = document.querySelector('.range-field__slider');
+    noUiSlider.create(rangeSlider, {
+        start: [75],
+        step: 1,
+        range: {
+            'min': [0],
+            'max': [100]
+        },
+        format: {
+            to: function (value) {
+                return value;
+            },
+            from: function (value) {
+                return Number(value);
+            }
+        }
+    });
+    var stepSliderValueElement = document.querySelector('.range-field__value');
+
+    rangeSlider.noUiSlider.on('update', function (values, handle) {
+        stepSliderValueElement.innerHTML = values[handle] + ' %';
+    });
+
+
+    // upload file
+    $('.file-field input').change(function(){
+        var $upload = $(this);
+        if ($upload[0].files[0]){
+            $('.file-field span').text($upload[0].files[0]['name']);
+        } else {
+            $('.file-field span').text('Прикрепить файл');
+        }
+
+    });
+});
